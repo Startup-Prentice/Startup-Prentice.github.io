f9657c8110af98d6445dff5e38dbfa2f77008aef
John Parsons <parsonsj@mit.edu>
2015-06-29 19:09:23 -0400

    Add barebones website

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/**
+ * index.js: JavaScript back-end for the Startup-Prentice homepage.
+ * Created by John on 6/29/15.
+ *
+ * Copyright (c) 2015 Startup-Prentice. All rights reserved.
+ */
+
+$(function () {
+
+});
